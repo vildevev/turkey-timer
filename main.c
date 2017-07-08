3ba8ef0b2f95a64899647d78223db005965cc51c
“Vilde <vilde@vevatne.no>
2017-07-07 22:28:44 -0700

    Initial commit

---
+++ main.c
@@ -0,0 +1,27 @@
+//
+//  main.c
+//  TurkeyTimer
+//
+//  Created by Vilde Vevatne on 17/04/2017.
+//  Copyright © 2017 Vilde Vevatne. All rights reserved.
+//
+
+#include <stdio.h>
+
+void showCookTimeForTurkey(int pounds)
+{
+    int necessaryMinutes = 15 + 15 * pounds;
+    printf("Cook for %d minutes.\n", necessaryMinutes);
+    if (necessaryMinutes > 120) {
+        int halfway = necessaryMinutes / 2;
+        printf("Rotate after %d of the %d minutes.\n", halfway, necessaryMinutes);
+    }
+}
+
+int main(int argc, const char * argv[]) {
+    int totalWeight = 10;
+    int gibletsWeight = 1;
+    int turkeyWeight = totalWeight - gibletsWeight;
+    showCookTimeForTurkey(turkeyWeight);
+    return 0;
+}
